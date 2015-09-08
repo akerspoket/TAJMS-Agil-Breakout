1283f90e0cc0151f8b36e05be6edc147cb98937c
konrad öhman <konrad_o_92@hotmail.com>
2015-09-08 11:19:18 +0200

    Not working

--- Breakout - Agil/Breakout - Agil/GraphicsEngine.h
+++ Breakout - Agil/Breakout - Agil/GraphicsEngine.h
@@ -1,10 +1,23 @@
 #pragma once
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "d3dx11.lib")
+#pragma comment(lib, "d3dx10.lib")
+
 #include <windows.h>
 #include <windowsx.h>
 #include <d3d11_1.h>
 #include <DirectXMath.h>
+#include <dxgi.h>
+#include <d3dcommon.h>
+#include <d3dcompiler.h>
 
-#pragma comment (lib, "d3d11.lib")
+struct Vertex
+{
+	float
+};
+
 class GraphicsEngine
 {
 public:
@@ -16,8 +29,10 @@ public:
 	ID3D11Device *dev;                   
 	ID3D11DeviceContext *devcon;         
 	ID3D11RenderTargetView *backbuffer;  
-										 
+	ID3D11VertexShader *pVS;    // the vertex shader
+	ID3D11PixelShader *pPS;     // the pixel shader									 
 	void InitD3D(HWND hWnd);     // sets 
+	void InitPipeline();
 	void CleanD3D(void);         // close
 	void RenderFrame(void);
 
