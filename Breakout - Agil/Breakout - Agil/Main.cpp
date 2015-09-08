caa547de749af4e996f080844df355086e089880
Eric Ahlström <yooric90@hotmail.com>
2015-09-08 16:31:21 +0200

    Cleanup of work in wrong branch

--- Breakout - Agil/Breakout - Agil/Main.cpp
+++ Breakout - Agil/Breakout - Agil/Main.cpp
@@ -1,17 +1,12 @@
 #include <iostream>
-#include "SystemGrejen.h"
-#include "Entiteter.h"
+
 using namespace std;
 
 int main()
 {
-	SystemGrejen systTest;
-	systTest.SysProg();
 	cout << "Hello world"<< endl;
 	int endOfMain;
 	cin >> endOfMain;
-	int hej = 1;
-	hej = 2 + 4+5+6;
 
 	return 0;
 }
