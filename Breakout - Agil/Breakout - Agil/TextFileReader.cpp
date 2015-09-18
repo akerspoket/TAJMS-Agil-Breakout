2adf8f7b102b24194d1e137adceb2672e77b44d3
john bergsten <putte_dt@hotmail.se>
2015-09-18 14:18:41 +0200

    Added a folder for Levels

---
+++ Breakout - Agil/Breakout - Agil/TextFileReader.cpp
@@ -0,0 +1,49 @@
+#include "TextFileReader.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+TextFileReader::TextFileReader()
+{
+
+}
+
+
+TextFileReader::~TextFileReader()
+{
+
+}
+
+void TextFileReader::WriteTextFile(string p_textName, string p_textAttSkriva)
+{
+
+	ofstream myfile;
+	myfile.open(p_textName);
+	myfile << p_textAttSkriva;
+	myfile.close();
+}
+
+void TextFileReader::ReadTextFile(string p_textName)
+{
+	string line;
+	//vector<string> o_text;
+	ifstream myfile(p_textName);
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			o_text.push_back(line);
+		}
+		myfile.close();
+	}
+	else
+	{
+		cout << "Unable to open file";
+	}
+	for (size_t i = 0; i < o_text.size(); i++)
+	{
+		cout << o_text[i];
+	}
+	
+}
