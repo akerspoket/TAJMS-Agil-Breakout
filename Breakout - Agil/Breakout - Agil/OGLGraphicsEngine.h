eca1e0f7671d512a277fd2ce3f5d2d084a234abe
Slowey <lucas.holmqvist@gmail.com>
2015-09-23 16:57:04 +0200

    Fixat länkingen i ogl. Startat med OGL strukturen. Finns stöd (limited) för uniform. Finns stöd (limited) för Vertexbufferskapning

--- Breakout - Agil/Breakout - Agil/OGLGraphicsEngine.h
+++ Breakout - Agil/Breakout - Agil/OGLGraphicsEngine.h
@@ -1,22 +1,60 @@
+#pragma once
+#include <GL\glew.h>
 #include <SDL2\SDL.h>
-#include <GL/glew.h>
+#include "glext.h"
 #include "Math.h"
 #include <iostream>
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <assert.h>
+#include <vector>
 
-#pragma once
+using namespace std;
+
+struct Vertexasd
+{
+	Vertexasd(vec3 pos, vec2 tex)
+	{
+		position = pos;
+		texcoord = tex;
+	}
+	vec3 position;
+	vec2 texcoord;
+};
+
+
 class OGLGraphicsEngine
 {
 public:
 	OGLGraphicsEngine();
 	~OGLGraphicsEngine();
 
-	int InitGlew();
+	int InitGlew(SDL_Window* pWND);
 	void CreateTriangle();
 	void RenderFrame();
+	void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType);
+	void CompileShaders();
+	
+
 
 	vec3 Vertices[3];
 	GLuint VBO;
 	SDL_Window* mWindow;
 	SDL_GLContext m_glContext;
+	const char* pVSFileName = "shader.vs";
+	const char* pFSFileName = "shader.fs";
+
+private:
+	bool ReadFile(const char* pFileName, string& outFile);
+	int CreateVertexBuffer(void* pDataStart, int pBufferSize);
+	int AddUniform(GLuint pShader, const GLchar* pName);
+	vector<GLuint> mBuffers;
+	int mVertexBufferID;
+	vector<GLuint> mUniforms;
+	int mUniformID;
+	GLuint normalShaderProg;
+
+
 };
 
