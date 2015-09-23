747842ec0de072775623e03fd2e091b8775092c2
john bergsten <putte_dt@hotmail.se>
2015-09-23 14:08:33 +0200

    LevelManager/INitialize  kankse fixat lite i generate level Gått tillbaks mot hårdkodning

--- Breakout - Agil/Breakout - Agil/LevelManager.cpp
+++ Breakout - Agil/Breakout - Agil/LevelManager.cpp
@@ -54,39 +54,54 @@ void LevelManager::Initialize()
 
 
 	///ska skapa en BluePrint
-	vector<string> mEntitiyVector;
-	mEntitiyVector = TextFileReader::ReadTextFile("Entities/Pad.txt");
-	string tBlueprintNamn = "";
-	//string TestSomSatan = "";
-	for (size_t i = 0; i < mEntitiyVector.size(); i++)
-	{
-		//TestSomSatan = mEntitiyVector[i];
-		if (mEntitiyVector[i].compare("xEntity") == 0)
-		{
-			++i;
-			tBlueprintNamn = mEntitiyVector[i];
-		}
-		else if (mEntitiyVector[i].compare("xTranform") == 0)
-		{
-			tBlueprint[TransformType] = tTrans;
-		}
-		else if (mEntitiyVector[i].compare("xMesh") == 0)
-		{
-			//++i;
-			//std::wstring name((L)mEntitiyVector[i]);
-			//const wchar_t* szName = name.c_str();
-			//
-			//tGraphicsInterFace->CreateTexture(mEntitiyVector[i]);
-			//++i;
-			//tGraphicsInterFace->CreateObject(mEntitiyVector[i]);
-			////tBlueprint[MeshType] = mEntitiyVector[i];
-		}
-		else if (mEntitiyVector[i].compare("xLabel"))
-		{
-			///////////Kolla vilken label det är i LabelComponent
 
-		}
-	}
+
+	////Med Textfil
+	//vector<string> mEntitiyVector;
+	//mEntitiyVector = TextFileReader::ReadTextFile("Entities/Pad.txt");
+	//string tBlueprintNamn = "";
+	//string TestSomSatan = "";
+	//for (size_t i = 0; i < mEntitiyVector.size(); i++)
+	//{
+	//	//TestSomSatan = mEntitiyVector[i];
+	//	if (mEntitiyVector[i].compare("xEntity") == 0)
+	//	{
+	//		++i;
+	//		tBlueprintNamn = mEntitiyVector[i];
+	//	}
+	//	else if (mEntitiyVector[i].compare("xTranform") == 0)
+	//	{
+	//		tBlueprint[TransformType] = tTrans;
+	//	}
+	//	else if (mEntitiyVector[i].compare("xMesh") == 0)
+	//	{
+	//		//++i;
+	//		//std::wstring name((L)mEntitiyVector[i]);
+	//		//const wchar_t* szName = name.c_str();
+	//		//
+	//		//tGraphicsInterFace->CreateTexture(mEntitiyVector[i]);
+	//		//++i;
+	//		//tGraphicsInterFace->CreateObject(mEntitiyVector[i]);
+	//		////tBlueprint[MeshType] = mEntitiyVector[i];
+	//	}
+	//	else if (mEntitiyVector[i].compare("xLabel"))
+	//	{
+	//		///////////Kolla vilken label det är i LabelComponent
+
+	//	}
+	//}
+
+
+
+
+
+
+
+
+
+
+
+
 	//set component values
 	//we ignore this and use the initialization values for test
 	//WARNINIG: We should call the graphic engine for loading a texture then
@@ -149,8 +164,16 @@ void LevelManager::GenerateWorld(string pWorldName)
 	GetComponent<VelocityComponent>(tNewID)->mSpeed = 0.001f;
 	
 	//Reading The level from textfile
-	//vector<string> mLevelTextVector;
-	//mLevelTextVector = TextFileReader::ReadTextFile(pWorldName);
+	vector<string> mLevelTextVector;
+	mLevelTextVector = TextFileReader::ReadTextFile(pWorldName);
+	string hej = "12345";
+	string hejsan;
+
+	for (size_t i = 0; i < mLevelTextVector[7].size(); i++)
+	{
+		hejsan+= mLevelTextVector[5].at(i);
+	}
+
 
 
 	//Create blocks
@@ -169,7 +192,7 @@ void LevelManager::GenerateWorld(string pWorldName)
 			tNewID = mEntityFactory->CreateEntity("Block");
 			tTrans = GetComponent<TransformComponent>(tNewID);
 			tTrans->mPosition.x = margin*i;
-			tTrans->mPosition.y = margin*j;
+			tTrans->mPosition.y = margin*j; 
 		}
 
 	//Create the ball
