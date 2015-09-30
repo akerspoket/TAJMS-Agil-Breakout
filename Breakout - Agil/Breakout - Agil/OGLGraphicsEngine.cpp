86a5efa06ebf57d6cc1c577e5e2c5ab8e98e6e39
Slowey <lucas.holmqvist@gmail.com>
2015-09-30 13:37:09 +0200

    Instanced Draw funkar!

--- Breakout - Agil/Breakout - Agil/OGLGraphicsEngine.cpp
+++ Breakout - Agil/Breakout - Agil/OGLGraphicsEngine.cpp
@@ -32,18 +32,14 @@ int OGLGraphicsEngine::InitGlew(SDL_Window* pWND)
 		return -1;
 	}
 	glEnable(GL_DEPTH_TEST);
-	//printf("GL version: %s\n", glGetString(GL_VERSION));
+	printf("GL version: %s\n", glGetString(GL_VERSION));
 	//CreateTriangle();
-	//Vertices = mObjLoader->LoadObj("bth.obj", 0.03f);
+	Vertices = mObjLoader->LoadObj("bth.obj", vec3(0.03f, 0.03f, 0.03f));
 
 
-	mVertexBufferID = CreateVertexBuffer(Vertices.data(), sizeof(Vertex)*Vertices.size());
+	mVertexBufferID = CreateObjectBuffer(Vertices.data(), sizeof(Vertex)*Vertices.size(), Vertices.size());
 	CompileShaders();
-	mUniformID = AddUniform(normalShaderProg, "gScale");
-
-	glm::mat4x4 tWorldMat;
-
-
+	InitGraphics(45.0f, 600.0f, 800.0f, 0.1f, 100, 13.0f);
 
 
 	return 1;
@@ -75,6 +71,19 @@ int OGLGraphicsEngine::CreateVertexBuffer(void* pDataStart, int pBufferSize)
 
 }
 
+int OGLGraphicsEngine::CreateObjectBuffer(void* pDataStart, int pDataSize,int pNumberOfIndices)
+{
+	GLuint tTempBuffer;
+	glGenBuffers(1, &tTempBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, tTempBuffer);
+	glBufferData(GL_ARRAY_BUFFER, pDataSize, pDataStart, GL_STATIC_DRAW);
+	ObjectBufferType tBT;
+	tBT.BufferHandle = tTempBuffer;
+	tBT.NumberOfIndices = pNumberOfIndices;
+	mObjectBuffers.push_back(tBT);
+	return mObjectBuffers.size() - 1;
+}
+
 int OGLGraphicsEngine::AddUniform(GLuint pShader, const GLchar* pName)
 {
 	glUseProgram(pShader);
@@ -94,23 +103,85 @@ void OGLGraphicsEngine::RenderFrame()
 	static float Scale = 0.0f;
 	Scale += 0.001f;
 
-	glUniform1f(mUniforms[mUniformID], sinf(Scale));
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glEnableVertexAttribArray(0);
-	glEnableVertexAttribArray(1);
-	glEnableVertexAttribArray(2);
-	glBindBuffer(GL_ARRAY_BUFFER, mBuffers[mVertexBufferID]);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(3 * sizeof(float)));
-	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(5 * sizeof(float)));
-	glDrawArrays(GL_TRIANGLES, 0, Vertices.size());
-	glDisableVertexAttribArray(0);
-	glDisableVertexAttribArray(1);
-	glDisableVertexAttribArray(2);
+	//glEnableVertexAttribArray(0);
+	//glEnableVertexAttribArray(1);
+	//glEnableVertexAttribArray(2);
+	//glBindBuffer(GL_ARRAY_BUFFER, mBuffers[mVertexBufferID]);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(3 * sizeof(float)));
+	//glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(5 * sizeof(float)));
+	//glDrawArrays(GL_TRIANGLES, 0, Vertices.size());
+	//glDisableVertexAttribArray(0);
+	//glDisableVertexAttribArray(1);
+	//glDisableVertexAttribArray(2);
+
+	mInstanceMatricesTemp.clear();
+	for (size_t i = 0; i < 4; i++)
+	{
+		InstanceBufferType tTransMat;
+		tTransMat.TranslationMatrices = glm::translate(glm::vec3((float)i*4.0f*sin(Scale), 1.0f, 1.0f));
+		mInstanceMatricesTemp.push_back(tTransMat);
+
+	}
+	
+
+	DrawObjects(mVertexBufferID, mInstanceMatricesTemp, 0);
 
 }
 
+void OGLGraphicsEngine::DrawObjects(int pMeshType, vector<InstanceBufferType> pInstanceBufferData, int pTextureBuffer)
+{
+
+	glBindBuffer(GL_ARRAY_BUFFER, mBuffers[mTransBuffer]);
+	glBufferData(GL_ARRAY_BUFFER, pInstanceBufferData.size()* sizeof(InstanceBufferType), pInstanceBufferData.data(), GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glEnableVertexAttribArray(1);
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, mObjectBuffers[pMeshType].BufferHandle);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), 0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(5 * sizeof(float)));
+	glBindBuffer(GL_ARRAY_BUFFER, mBuffers[mTransBuffer]);
+	for (size_t i = 0; i < 4; i++)
+	{
+		glEnableVertexAttribArray(3 + i);
+		glVertexAttribPointer(3+i, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)*i));
+		glVertexAttribDivisor(3 + i, 1);
+	}	
+	glDrawArraysInstanced(GL_TRIANGLES, 0, mObjectBuffers[pMeshType].NumberOfIndices, pInstanceBufferData.size());
+	glDisableVertexAttribArray(0);
+	glDisableVertexAttribArray(1);
+	glDisableVertexAttribArray(2);
+	glDisableVertexAttribArray(3);
+	glDisableVertexAttribArray(4);
+	glDisableVertexAttribArray(5);
+	glDisableVertexAttribArray(6);
+
+}
+
+
+
+void OGLGraphicsEngine::InitGraphics(float pFoVAngleY, float pHeight, float pWidth, float pNear, float pFar, float pZPos)
+{
+	mWVPBuffer.world = glm::mat4();
+	mWVPBuffer.view = glm::lookAt(glm::vec3(0.0f, 0.0f, pZPos), glm::vec3(0.0f, 0.0f, -1.0f), glm::vec3(0, 1, 0));
+	mWVPBuffer.projection = glm::perspective(pFoVAngleY, pWidth / pHeight, pNear, pFar);
+
+	mWVPBufferID = AddUniform(mNormalShaderProg, "gWorld");
+	glUniformMatrix4fv(mUniforms[mWVPBufferID], 1, GL_FALSE, (float*)&mWVPBuffer.world);
+	mWVPBufferID = AddUniform(mNormalShaderProg, "gView");
+	glUniformMatrix4fv(mUniforms[mWVPBufferID], 1, GL_FALSE, (float*)&mWVPBuffer.view);
+	mWVPBufferID = AddUniform(mNormalShaderProg, "gProj");
+	glUniformMatrix4fv(mUniforms[mWVPBufferID], 1, GL_FALSE, (float*)&mWVPBuffer.projection);
+	
+	//För instanceDraw
+	mTransBuffer = CreateVertexBuffer(nullptr, 0);
+
+
+}
 
 
 void OGLGraphicsEngine::AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
@@ -119,7 +190,8 @@ void OGLGraphicsEngine::AddShader(GLuint ShaderProgram, const char* pShaderText,
 
 	if (ShaderObj == 0) {
 		fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-		exit(0);
+		int hi;
+		std::cin >> hi;
 	}
 
 	const GLchar* p[1];
@@ -134,7 +206,8 @@ void OGLGraphicsEngine::AddShader(GLuint ShaderProgram, const char* pShaderText,
 		GLchar InfoLog[1024];
 		glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
 		fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
-		exit(1);
+		int hi;
+		std::cin >> hi;
 	}
 
 	glAttachShader(ShaderProgram, ShaderObj);
@@ -142,11 +215,12 @@ void OGLGraphicsEngine::AddShader(GLuint ShaderProgram, const char* pShaderText,
 
 void OGLGraphicsEngine::CompileShaders()
 {
-	normalShaderProg = glCreateProgram();
+	mNormalShaderProg = glCreateProgram();
 
-	if (normalShaderProg == 0) {
+	if (mNormalShaderProg == 0) {
 		fprintf(stderr, "Error creating shader program\n");
-		exit(1);
+		int hi;
+		std::cin >> hi;
 	}
 
 	std::string vs, fs;
@@ -159,29 +233,31 @@ void OGLGraphicsEngine::CompileShaders()
 		exit(1);
 	};
 
-	AddShader(normalShaderProg, vs.c_str(), GL_VERTEX_SHADER);
-	AddShader(normalShaderProg, fs.c_str(), GL_FRAGMENT_SHADER);
+	AddShader(mNormalShaderProg, vs.c_str(), GL_VERTEX_SHADER);
+	AddShader(mNormalShaderProg, fs.c_str(), GL_FRAGMENT_SHADER);
 
 	GLint Success = 0;
 	GLchar ErrorLog[1024] = { 0 };
 
-	glLinkProgram(normalShaderProg);
-	glGetProgramiv(normalShaderProg, GL_LINK_STATUS, &Success);
+	glLinkProgram(mNormalShaderProg);
+	glGetProgramiv(mNormalShaderProg, GL_LINK_STATUS, &Success);
 	if (Success == 0) {
-		glGetProgramInfoLog(normalShaderProg, sizeof(ErrorLog), NULL, ErrorLog);
+		glGetProgramInfoLog(mNormalShaderProg, sizeof(ErrorLog), NULL, ErrorLog);
 		fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
-		exit(1);
+		int hi;
+		std::cin >> hi;
 	}
 
-	glValidateProgram(normalShaderProg);
-	glGetProgramiv(normalShaderProg, GL_VALIDATE_STATUS, &Success);
+	glValidateProgram(mNormalShaderProg);
+	glGetProgramiv(mNormalShaderProg, GL_VALIDATE_STATUS, &Success);
 	if (!Success) {
-		glGetProgramInfoLog(normalShaderProg, sizeof(ErrorLog), NULL, ErrorLog);
+		glGetProgramInfoLog(mNormalShaderProg, sizeof(ErrorLog), NULL, ErrorLog);
 		fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
-		exit(1);
+		int hi;
+		std::cin >> hi;
 	}
 
-	glUseProgram(normalShaderProg);
+	glUseProgram(mNormalShaderProg);
 }
 
 bool OGLGraphicsEngine::ReadFile(const char* pFileName, string& outFile)
