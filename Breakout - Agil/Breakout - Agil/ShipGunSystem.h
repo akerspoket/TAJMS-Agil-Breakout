809d726f2cbe829bed7574b61ae2884988a49103
Eric Ahlström <yooric90@hotmail.com>
2015-10-21 09:01:29 +0200

    LevelManager: Now loads cannon OBJ ShipGunSystem: now implemented. Might need further adjustments. REALLY bad system. Dependant on only ONE pad, ball AND cannon GraphicsInterface: Quaternion rotation didn't work out too well so now it just rotates around z-axis with a provided angle

--- Breakout - Agil/Breakout - Agil/ShipGunSystem.h
+++ Breakout - Agil/Breakout - Agil/ShipGunSystem.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "System.h"
+#include "Math.h"
+
 class ShipGunSystem :
 	public System
 {
@@ -15,5 +17,15 @@ public:
 	void Pause();
 	void Stop();
 	void OnEvent(Event* pEvent);
+
+private:
+	vec3 CalculateCannonDirection(vec2 ballIntersectPos, vec2 pBallPos, vec2 pPadPos);
+
+	vec2 mBallIntersectPos;
+	float mCannonFollowSpeed;
+	float mCannonPos;
+
+	float mCannonAngle;
+	float mCannonAngleFollowSpeed;
 };
 
